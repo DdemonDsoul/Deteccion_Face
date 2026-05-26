5ec2683a4dd02a7e07255b8f7164863caad58efb
Demond <106361952+DdemonDsoul@users.noreply.github.com>
2026-05-26 13:36:47 -0600

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -1,18 +1,12 @@
+# preproces.py
 from PIL import Image
 import numpy as np
-
-IMG_SIZE = 128
+from config import IMG_SIZE
 
 def procesar_imagen(ruta_imagen):
-
     img = Image.open(ruta_imagen)
-
     img = img.convert("RGB")
-
     img = img.resize((IMG_SIZE, IMG_SIZE))
-
-    img = np.array(img)
-
-    img = img / 255.0
-
+    img = np.array(img) / 255.0
+    img = np.expand_dims(img, axis=0)  # Formato final listo: (1, 128, 128, 3)
     return img
